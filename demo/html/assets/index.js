7b1337628cdf0c6709ee671e548491f90e878948
oy <337200337@qq.com>
2024-06-03 16:15:14 +0800

    号码

--- demo/html/assets/index.js
+++ demo/html/assets/index.js
@@ -2,7 +2,7 @@
 var serverAddr = "https://120.26.125.181";
 var ttsServer = "https://120.26.125.181/tts";
 
-serverAddr = "https://192.168.3.118:8888";
+serverAddr = "https://120.26.125.181";
 
 /**
  * 子页面名称
